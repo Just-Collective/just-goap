0a843545faa5a351628bd07a3ff0ce60ed9289e4
bvanseg <bvanseg@gmail.com>
2025-09-24 18:09:43 -0400

    📝 Added comments to GOAPPlanner.
    Signed-off-by: = <bvanseg@gmail.com>

--- src/main/java/com/just/goap/plan/GOAPPlanner.java
+++ src/main/java/com/just/goap/plan/GOAPPlanner.java
@@ -31,6 +31,8 @@ public class GOAPPlanner<T> {
             var plan = buildPlanForConditions(desiredConditions, currentState);
 
             if (plan != null && !plan.isEmpty()) {
+                // FIXME: The cost-per-action here is evaluated using the wrong world state. We need to use the
+                // simulated state.
                 var cost = plan.stream()
                     .map(action -> action.getCost(context, currentState))
                     .reduce(0.0f, Float::sum);
@@ -50,17 +52,26 @@ public class GOAPPlanner<T> {
         GOAPWorldState currentState
     ) {
         if (currentState.satisfies(desiredConditions)) {
+            // The current world state already satisfies the desired conditions, there are no further actions necessary
+            // to achieve the desired conditions.
             return List.of();
         }
 
+        // Prepare a new plan (sequential list of actions).
         var plan = new ArrayList<GOAPAction<T>>();
+        // Create a working world state. We will use this to "simulate" the effects of actions on the world state.
         var workingState = new GOAPMutableWorldState(currentState);
 
+        // For every desired condition, we need to figure out if the condition is already satisfied.
         for (var condition : desiredConditions.getConditions()) {
             if (condition.satisfiedBy(workingState)) {
+                // The condition is already satisfied, no further actions necessary to try and satisfy it.
                 continue;
             }
 
+            // The current condition is not satisfied by the working world state, so we now need to find all available
+            // actions that can satisfy this condition.
+            // TODO: For a given condition, we should be able to compute a O(1) lookup map of actions that satisfy it.
             var satisfyingActions = goap.getAvailableActions()
                 .stream()
                 .filter(action -> condition.satisfiedBy(action.getEffects()))
@@ -68,12 +79,21 @@ public class GOAPPlanner<T> {
 
             var satisfied = false;
 
+            // For the given desired condition, we may have multiple actions that can potentially satisfy the desired
+            // condition. So we need to go through all of them and see which actions are actually viable, if any.
+            // FIXME: This code will pick the first action with preconditions that can be satisfied.
+            // FIXME: As a consequence, there is no evaluation of which action has a lower cost.
             for (var action : satisfyingActions) {
+                // Build a subplan for the current action. Every action has preconditions, and like our original
+                // desired conditions, we need to make sure we have a viable plan to solve those preconditions.
                 var subPlan = buildPlanForConditions(action.getPreconditions(), workingState);
 
                 if (subPlan != null) {
+                    // If sub plan is valid, it will be non-null. We can add all of its actions to our current plan.
                     plan.addAll(subPlan);
+                    // The plan to arrive at our current action was valid, so the action itself is also valid. Add it.
                     plan.add(action);
+                    // Update our working state with the action's effects to simulate the action being done.
                     workingState.apply(action.getEffects());
                     satisfied = true;
                     break;
@@ -81,6 +101,7 @@ public class GOAPPlanner<T> {
             }
 
             if (!satisfied) {
+                // There was no action that could satisfy the current condition, so return null.
                 return null;
             }
         }
