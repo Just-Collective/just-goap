bcb58e289ccee1888720d1d94c89d310b92f7a01
bvanseg <bvanseg@gmail.com>
2025-09-26 18:31:49 -0400

    ☢️♻️ Goal is no longer abstract, now uses builder pattern.
    Signed-off-by: = <bvanseg@gmail.com>

--- src/main/java/com/just/goap/Goal.java
+++ src/main/java/com/just/goap/Goal.java
@@ -1,19 +1,34 @@
 package com.just.goap;
 
+import com.just.goap.condition.Condition;
 import com.just.goap.condition.ConditionContainer;
+import com.just.goap.condition.expression.Expression;
 
-public abstract class Goal {
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Goal {
+
+    public static <T> Goal of(TypedIdentifier<? super T> identifier, Expression<? super T> expression) {
+        return of(new Condition<T>(identifier, expression));
+    }
+
+    public static Goal of(Condition<?> condition) {
+        return builder(condition).build();
+    }
+
+    public static Builder builder(Condition<?> condition) {
+        return new Builder(condition);
+    }
 
     private final ConditionContainer desiredConditions;
 
     private final String name;
 
-    protected Goal() {
-        this.desiredConditions = createDesiredConditions();
-        this.name = this.getClass().getSimpleName();
+    private Goal(ConditionContainer conditionContainer, String name) {
+        this.desiredConditions = conditionContainer;
+        this.name = name;
     }
-
-    protected abstract ConditionContainer createDesiredConditions();
 
     public ConditionContainer getDesiredConditions() {
         return desiredConditions;
@@ -26,5 +41,33 @@ public abstract class Goal {
     @Override
     public String toString() {
         return getName();
+    }
+
+    public static class Builder {
+
+        private final List<Condition<?>> desiredConditions;
+
+        private String name;
+
+        private Builder(Condition<?> condition) {
+            this.desiredConditions = new ArrayList<>();
+            this.name = this.getClass().getSimpleName();
+
+            desiredConditions.add(condition);
+        }
+
+        public Builder addDesiredCondition(Condition<?> condition) {
+            desiredConditions.add(condition);
+            return this;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Goal build() {
+            return new Goal(ConditionContainer.of(desiredConditions), name);
+        }
     }
 }
