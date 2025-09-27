0d9d896294e29d7d4026e284e919853c3cb7b08f
bvanseg <bvanseg@gmail.com>
2025-09-27 02:40:01 -0400

    ✨ Added ReadableWorldState.
    Signed-off-by: = <bvanseg@gmail.com>

---
+++ src/main/java/com/just/goap/state/ReadableWorldState.java
@@ -0,0 +1,39 @@
+package com.just.goap.state;
+
+import com.just.goap.TypedIdentifier;
+import com.just.goap.condition.ConditionContainer;
+import com.just.goap.effect.EffectContainer;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
+
+public interface ReadableWorldState {
+
+    Map<TypedIdentifier<?>, ?> getMap();
+
+    @SuppressWarnings("unchecked")
+    default <T> @Nullable T getOrNull(TypedIdentifier<T> key) {
+        return (T) getMap().get(key);
+    }
+
+    default boolean satisfiedBy(ReadableWorldState worldState) {
+        for (var entry : getMap().entrySet()) {
+            var otherValue = worldState.getOrNull(entry.getKey());
+
+            if (!entry.getValue().equals(otherValue)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    default boolean satisfies(EffectContainer effectContainer) {
+        return effectContainer.toWorldState()
+            .satisfiedBy(this);
+    }
+
+    default boolean satisfies(ConditionContainer conditionContainer) {
+        return conditionContainer.satisfiedBy(this);
+    }
+}
