80e95d47163a5205eccaff0ee2fde2553e2982db
bvanseg <bvanseg@gmail.com>
2025-09-30 14:52:47 -0400

    ♻️ Impl Satisfiable for Condition.
    Signed-off-by: = <bvanseg@gmail.com>

--- src/main/java/com/just/goap/condition/Condition.java
+++ src/main/java/com/just/goap/condition/Condition.java
@@ -1,22 +1,24 @@
 package com.just.goap.condition;
 
 import com.just.goap.GOAPKey;
+import com.just.goap.Satisfiable;
 import com.just.goap.condition.expression.Expression;
 import com.just.goap.effect.EffectContainer;
 import com.just.goap.state.ReadableWorldState;
 
 public record Condition<T>(
-    GOAPKey<? super T> key,
+    GOAPKey<T> key,
     Expression<? super T> expression
-) {
+) implements Satisfiable {
 
+    @Override
     public boolean satisfiedBy(EffectContainer effectContainer) {
         return satisfiedBy(effectContainer.toWorldState());
     }
 
-    @SuppressWarnings("unchecked")
+    @Override
     public boolean satisfiedBy(ReadableWorldState worldState) {
-        var value = (T) worldState.getOrNull(key);
+        var value = worldState.getOrNull(key);
         return value != null && expression.evaluate(value);
     }
 }
