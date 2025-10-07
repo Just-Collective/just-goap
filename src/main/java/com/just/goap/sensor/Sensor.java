fe3c93fd756d3a55634b6362462972fc595dd117
bvanseg <bvanseg@gmail.com>
2025-10-07 02:44:49 -0400

    ☢️♻️ Refactor Mono and Multi classes as subclasses of Sensor.
    Signed-off-by: = <bvanseg@gmail.com>

--- src/main/java/com/just/goap/sensor/Sensor.java
+++ src/main/java/com/just/goap/sensor/Sensor.java
@@ -12,7 +12,7 @@ import com.just.goap.state.ReadableWorldState;
 import java.util.Map;
 import java.util.Set;
 
-public sealed interface Sensor<T> permits Mono, Multi {
+public sealed interface Sensor<T> {
 
     static <T, I1, O1> Mono.Compose<T, I1, O1> compose(
         StateKey.Sensed<I1> sourceKey,
@@ -133,4 +133,117 @@ public sealed interface Sensor<T> permits Mono, Multi {
 
     Set<StateKey.Sensed<?>> outputKeys();
 
+    sealed interface Mono<T, O> extends Sensor<T> {
+
+        StateKey.Sensed<O> key();
+
+        @Override
+        default Set<StateKey.Sensed<?>> outputKeys() {
+            return Set.of(key());
+        }
+
+        record Map<T, O1>(
+            StateKey.Sensed<O1> key,
+            Function<? super T, ? extends O1> extractor
+        ) implements Mono<T, O1> {}
+
+        record Compose<T, I1, O1>(
+            StateKey.Sensed<I1> sourceKey,
+            StateKey.Sensed<O1> key,
+            Function2<? super T, ? super I1, ? extends O1> extractor
+        ) implements Mono<T, O1> {}
+
+        record Compose2<T, I1, I2, O1>(
+            StateKey.Sensed<I1> sourceKeyA,
+            StateKey.Sensed<I2> sourceKeyB,
+            StateKey.Sensed<O1> key,
+            Function3<? super T, ? super I1, ? super I2, ? extends O1> extractor
+        ) implements Mono<T, O1> {}
+
+        record Compose3<T, I1, I2, I3, O1>(
+            StateKey.Sensed<I1> sourceKeyA,
+            StateKey.Sensed<I2> sourceKeyB,
+            StateKey.Sensed<I3> sourceKeyC,
+            StateKey.Sensed<O1> key,
+            Function4<? super T, ? super I1, ? super I2, ? super I3, ? extends O1> extractor
+        ) implements Mono<T, O1> {}
+
+        record LazyCompose<T, W extends ReadableWorldState, O1>(
+            StateKey.Sensed<O1> outputKeyA,
+            Function2<? super T, ? super W, ? extends java.util.Map<StateKey<?>, ?>> extractor
+        ) implements Mono<T, O1> {
+
+            @Override
+            public StateKey.Sensed<O1> key() {
+                return outputKeyA;
+            }
+        }
+    }
+
+    sealed interface Multi<T> extends Sensor<T> {
+
+        record Decompose2<T, O1, O2>(
+            StateKey.Sensed<O1> outputKeyA,
+            StateKey.Sensed<O2> outputKeyB,
+            Function<? super T, ? extends Map<StateKey<?>, ?>> extractor
+        ) implements Multi<T> {
+
+            @Override
+            public Set<StateKey.Sensed<?>> outputKeys() {
+                return Set.of(outputKeyA, outputKeyB);
+            }
+        }
+
+        record Decompose3<T, O1, O2, O3>(
+            StateKey.Sensed<O1> outputKeyA,
+            StateKey.Sensed<O2> outputKeyB,
+            StateKey.Sensed<O3> outputKeyC,
+            Function<? super T, ? extends Map<StateKey<?>, ?>> extractor
+        ) implements Multi<T> {
+
+            @Override
+            public Set<StateKey.Sensed<?>> outputKeys() {
+                return Set.of(outputKeyA, outputKeyB, outputKeyC);
+            }
+        }
+
+        record LazyDecompose2<T, W extends ReadableWorldState, O1, O2>(
+            StateKey.Sensed<O1> outputKeyA,
+            StateKey.Sensed<O2> outputKeyB,
+            Function2<? super T, ? super W, ? extends Map<StateKey<?>, ?>> extractor
+        ) implements Multi<T> {
+
+            @Override
+            public Set<StateKey.Sensed<?>> outputKeys() {
+                return Set.of(outputKeyA, outputKeyB);
+            }
+        }
+
+        record Map1To2<T, I1, O1, O2>(
+            StateKey.Sensed<I1> sourceKeyA,
+            StateKey.Sensed<O1> outputKeyA,
+            StateKey.Sensed<O2> outputKeyB,
+            Function2<? super T, ? super I1, ? extends Map<StateKey<?>, ?>> extractor
+        ) implements Multi<T> {
+
+            @Override
+            public Set<StateKey.Sensed<?>> outputKeys() {
+                return Set.of(outputKeyA, outputKeyB);
+            }
+        }
+
+        record Map2To2<T, I1, I2, O1, O2>(
+            StateKey.Sensed<I1> sourceKeyA,
+            StateKey.Sensed<I2> sourceKeyB,
+            StateKey.Sensed<O1> outputKeyA,
+            StateKey.Sensed<O2> outputKeyB,
+            Function3<? super T, ? super I1, ? super I2, ? extends Map<StateKey<?>, ?>> extractor
+        ) implements Multi<T> {
+
+            @Override
+            public Set<StateKey.Sensed<?>> outputKeys() {
+                return Set.of(outputKeyA, outputKeyB);
+            }
+        }
+    }
 }
--- src/main/java/com/just/goap/state/SensingMutableWorldState.java
+++ src/main/java/com/just/goap/state/SensingMutableWorldState.java
@@ -2,8 +2,6 @@ package com.just.goap.state;
 
 import com.just.goap.StateKey;
 import com.just.goap.effect.EffectContainer;
-import com.just.goap.sensor.Mono;
-import com.just.goap.sensor.Multi;
 import com.just.goap.sensor.Sensor;
 
 import java.util.HashMap;
@@ -41,8 +39,8 @@ public final class SensingMutableWorldState<T> implements WorldState {
             var sensor = (Sensor<T>) sensorMap.get(key);
 
             value = switch (sensor) {
-                case Mono<T, ?> mono -> extractValueFromMonoSensor(key, mono);
-                case Multi<T> multi -> extractValueFromMultiSensor(key, multi);
+                case Sensor.Mono<T, ?> mono -> extractValueFromMonoSensor(key, mono);
+                case Sensor.Multi<T> multi -> extractValueFromMultiSensor(key, multi);
             };
 
             set(key, value);
@@ -51,37 +49,37 @@ public final class SensingMutableWorldState<T> implements WorldState {
         return value;
     }
 
-    private <O> O extractValueFromMonoSensor(StateKey<O> key, Mono<T, ?> mono) {
+    private <O> O extractValueFromMonoSensor(StateKey<O> key, Sensor.Mono<T, ?> mono) {
         return switch (mono) {
-            case Mono.LazyCompose<T, ?, ?> lazyCompose -> {
+            case Sensor.Mono.LazyCompose<T, ?, ?> lazyCompose -> {
                 @SuppressWarnings("unchecked")
-                var castedSensor = (Mono.LazyCompose<T, ReadableWorldState, Object>) lazyCompose;
+                var castedSensor = (Sensor.Mono.LazyCompose<T, ReadableWorldState, Object>) lazyCompose;
                 var resultMap = castedSensor.extractor().apply(context, this);
                 @SuppressWarnings("unchecked")
                 var castedValue = (O) resultMap.get(key);
                 yield castedValue;
             }
-            case Mono.Map<T, ?> map -> {
+            case Sensor.Mono.Map<T, ?> map -> {
                 @SuppressWarnings("unchecked")
-                var castedSensor = (Mono.Map<T, O>) map;
+                var castedSensor = (Sensor.Mono.Map<T, O>) map;
                 yield castedSensor.extractor().apply(context);
             }
-            case Mono.Compose<T, ?, ?> compose -> {
+            case Sensor.Mono.Compose<T, ?, ?> compose -> {
                 @SuppressWarnings("unchecked")
-                var castedSensor = (Mono.Compose<T, Object, O>) compose;
+                var castedSensor = (Sensor.Mono.Compose<T, Object, O>) compose;
                 var sourceValue = getOrNull(castedSensor.sourceKey());
                 yield castedSensor.extractor().apply(context, sourceValue);
             }
-            case Mono.Compose2<T, ?, ?, ?> compose2 -> {
+            case Sensor.Mono.Compose2<T, ?, ?, ?> compose2 -> {
                 @SuppressWarnings("unchecked")
-                var castedSensor = (Mono.Compose2<T, Object, Object, O>) compose2;
+                var castedSensor = (Sensor.Mono.Compose2<T, Object, Object, O>) compose2;
                 var sourceValueA = getOrNull(castedSensor.sourceKeyA());
                 var sourceValueB = getOrNull(castedSensor.sourceKeyB());
                 yield castedSensor.extractor().apply(context, sourceValueA, sourceValueB);
             }
-            case Mono.Compose3<T, ?, ?, ?, ?> compose3 -> {
+            case Sensor.Mono.Compose3<T, ?, ?, ?, ?> compose3 -> {
                 @SuppressWarnings("unchecked")
-                var castedSensor = (Mono.Compose3<T, Object, Object, Object, O>) compose3;
+                var castedSensor = (Sensor.Mono.Compose3<T, Object, Object, Object, O>) compose3;
                 var sourceValueA = getOrNull(castedSensor.sourceKeyA());
                 var sourceValueB = getOrNull(castedSensor.sourceKeyB());
                 var sourceValueC = getOrNull(castedSensor.sourceKeyC());
@@ -90,45 +88,45 @@ public final class SensingMutableWorldState<T> implements WorldState {
         };
     }
 
-    private <O> O extractValueFromMultiSensor(StateKey<O> key, Multi<T> multi) {
+    private <O> O extractValueFromMultiSensor(StateKey<O> key, Sensor.Multi<T> multi) {
         return switch (multi) {
-            case Multi.Decompose2<T, ?, ?> decompose2 -> {
+            case Sensor.Multi.Decompose2<T, ?, ?> decompose2 -> {
                 @SuppressWarnings("unchecked")
-                var castedSensor = (Multi.Decompose2<T, Object, Object>) decompose2;
+                var castedSensor = (Sensor.Multi.Decompose2<T, Object, Object>) decompose2;
                 var resultMap = castedSensor.extractor().apply(context);
                 @SuppressWarnings("unchecked")
                 var castedValue = (O) resultMap.get(key);
                 yield castedValue;
             }
-            case Multi.Decompose3<T, ?, ?, ?> decompose3 -> {
+            case Sensor.Multi.Decompose3<T, ?, ?, ?> decompose3 -> {
                 @SuppressWarnings("unchecked")
-                var castedSensor = (Multi.Decompose3<T, Object, Object, Object>) decompose3;
+                var castedSensor = (Sensor.Multi.Decompose3<T, Object, Object, Object>) decompose3;
                 var resultMap = castedSensor.extractor().apply(context);
                 @SuppressWarnings("unchecked")
                 var castedValue = (O) resultMap.get(key);
                 yield castedValue;
             }
-            case Multi.LazyDecompose2<T, ?, ?, ?> lazyDecompose2 -> {
+            case Sensor.Multi.LazyDecompose2<T, ?, ?, ?> lazyDecompose2 -> {
                 @SuppressWarnings("unchecked")
                 var castedSensor =
-                    (Multi.LazyDecompose2<T, ReadableWorldState, Object, Object>) lazyDecompose2;
+                    (Sensor.Multi.LazyDecompose2<T, ReadableWorldState, Object, Object>) lazyDecompose2;
                 var resultMap = castedSensor.extractor().apply(context, this);
                 @SuppressWarnings("unchecked")
                 var castedValue = (O) resultMap.get(key);
                 yield castedValue;
             }
-            case Multi.Map1To2<T, ?, ?, ?> map1To2 -> {
+            case Sensor.Multi.Map1To2<T, ?, ?, ?> map1To2 -> {
                 @SuppressWarnings("unchecked")
-                var castedSensor = (Multi.Map1To2<T, Object, Object, Object>) map1To2;
+                var castedSensor = (Sensor.Multi.Map1To2<T, Object, Object, Object>) map1To2;
                 var sourceValueA = getOrNull(castedSensor.sourceKeyA());
                 var resultMap = castedSensor.extractor().apply(context, sourceValueA);
                 @SuppressWarnings("unchecked")
                 var castedValue = (O) resultMap.get(key);
                 yield castedValue;
             }
-            case Multi.Map2To2<T, ?, ?, ?, ?> map2To2 -> {
+            case Sensor.Multi.Map2To2<T, ?, ?, ?, ?> map2To2 -> {
                 @SuppressWarnings("unchecked")
-                var castedSensor = (Multi.Map2To2<T, Object, Object, Object, Object>) map2To2;
+                var castedSensor = (Sensor.Multi.Map2To2<T, Object, Object, Object, Object>) map2To2;
                 var sourceValueA = getOrNull(castedSensor.sourceKeyA());
                 var sourceValueB = getOrNull(castedSensor.sourceKeyB());
                 var resultMap = castedSensor.extractor().apply(context, sourceValueA, sourceValueB);
